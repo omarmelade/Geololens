6ff95f309bc348399a215b2885034edf53bf78d5
Léo <58537513+leolaf@users.noreply.github.com>
2021-12-08 14:59:25 +0100

    ouhouhouh

--- Assets/Script/SetColor.cs
+++ Assets/Script/SetColor.cs
@@ -7,7 +7,6 @@ public class SetColor : MonoBehaviour
     
     Color green = Color.green;
     Color red = Color.white;
-    public GameObject obj;
 
     public bool selected;
     public float minWidth;
@@ -16,60 +15,40 @@ public class SetColor : MonoBehaviour
     public void SelectColor()
     {
         selected = true;
-        //GameObject[] objs = new GameObject[];
-        StartCoroutine("SelectColor");
-
+        StartCoroutine("Selected");
     }
 
     public void UnSelectColor()
     {
-        if (obj.transform.childCount > 0)
-        {
-
-            var mesh = obj.GetComponentsInChildren<MeshRenderer>();
-            for (int i = 0; i < mesh.Length; i++)
-            {
-                mesh[i].material.color = red;
-            }
-
-        }
-        else
-        {
-            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-            meshRenderer.material.color = red;
-        }
+        
+        selected = false;
+        StartCoroutine("Unselected");
     }
 
-    IEnumerator Selected(GameObject[] objs){
-        float currentWidth = minWidth;
-        float step = (maxWidth-minWidth)/100.0f;
+    IEnumerator Selected(){
+        float currentWidth = this.GetComponent<Outline>().OutlineWidth;
+        float step = (maxWidth-minWidth)/10.0f;
         while (selected && currentWidth<maxWidth)
         {
             currentWidth += step;
             if(currentWidth>maxWidth){
                 currentWidth = maxWidth;
             }
-            foreach (GameObject obj in objs)
-            {
-                obj.GetComponent<Outline>().OutlineWidth = currentWidth;
-            }
+            this.GetComponent<Outline>().OutlineWidth = currentWidth;
             yield return new WaitForSeconds(0.01f);
         }
     }
 
-    IEnumerator Unselected(GameObject[] objs){
-        float currentWidth = minWidth;
-        float step = (maxWidth-minWidth)/100.0f;
+    IEnumerator Unselected(){
+        float currentWidth = this.GetComponent<Outline>().OutlineWidth;
+        float step = (maxWidth-minWidth)/10.0f;
         while (!selected && currentWidth>minWidth)
         {
             currentWidth -= step;
             if(currentWidth<minWidth){
                 currentWidth = minWidth;
             }
-            foreach (GameObject obj in objs)
-            {
-                obj.GetComponent<Outline>().OutlineWidth = currentWidth;
-            }
+            this.GetComponent<Outline>().OutlineWidth = currentWidth;
             yield return new WaitForSeconds(0.01f);
         }
     }
