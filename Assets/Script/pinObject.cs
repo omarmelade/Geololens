693afbf4057ceb39998cad63b2b4f69b80b3c323
Omar <omarkhial74@gmail.com>
2021-12-09 10:41:03 +0100

    balise a moving to closest point

--- Assets/Script/pinObject.cs
+++ Assets/Script/pinObject.cs
@@ -16,21 +16,37 @@ public class pinObject : MonoBehaviour
             vertices = mesh.vertices;
             rend = other.gameObject.GetComponent<Renderer>();
 
-            Debug.Log(rend.bounds.min);
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                if (vertices[i].y == rend.bounds.min.y)
-                {
-                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    cube.transform.position = vertices[i];
+            List<Vector3> corners = new List<Vector3>();
 
-                }
-                if (vertices[i].y <= rend.bounds.min.y+1)
+            // get all corners
+            for (int i = 0; i < vertices.Length; i++)
+            { 
+                if (vertices[i].y < rend.bounds.min.y + 1f)
                 {
                     GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    sphere.transform.position = vertices[i];
+                    sphere.transform.position = vertices[i] + other.transform.position;
+
+                    Vector3 cornPos = vertices[i] + other.transform.position;
+                    corners.Add(cornPos);
                 }
             }
+            
+            // check the shortest dist 
+            float shortestDist = Vector3.Distance(corners[0], this.transform.position);
+            int shortestIndex = 0;
+            for (int j = 0; j < corners.Count; j++)
+            {
+                float dist = Vector3.Distance(corners[j], this.transform.position);
+                if (dist < shortestDist)
+                {
+                    shortestIndex = j;
+                    shortestDist = dist;
+                }
+            }
+
+            // set the gameobject to the shortest corner
+            this.transform.position = corners[shortestIndex];
+
             isPinned = true;
             print("pinned");
         }
