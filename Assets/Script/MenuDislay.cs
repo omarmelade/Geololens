a7baca2554fc6f828bd88c46d5272442dc31bd24
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-03-23 17:00:02 +0100

    Arborescence refonte

--- Assets/Script/MenuDislay.cs
+++ Assets/Script/MenuDislay.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -19,23 +19,14 @@ public class MenuDislay : MonoBehaviour
 
     public void maj_arbo()
     {
-        int numberOfChild = 5;
-        menu.transform.rotation = new Quaternion(0, 0, 0, 0);
-        Debug.Log(menu.transform.childCount);
-        for (int i = menu.transform.childCount; i > numberOfChild; i--)
-        {
-            Debug.Log("Suppression");
-            Destroy(menu.transform.GetChild(i-1).gameObject);
-        }
-        Debug.Log(menu.transform.childCount);
-        Debug.Log("On recrée les objets");
-        Transform ArborescenceBalise = menu.transform.GetChild(4);
-        string s = "";
-        float y = 0.0f;
+        suppressionArbo();
+        var tree = GameObject.FindGameObjectsWithTag("Virtual Scene")[0].GetComponent<VirtualObjectScene>().GetVirtualTree();
         int nb_go = 0;
+        string indent = "";
+        creationArbo(tree, indent, nb_go);
 
         //Code de mise ? jour Arborescence
-        List<ObjetScene> listeObjet = new List<ObjetScene>();
+        /*List<ObjetScene> listeObjet = new List<ObjetScene>();
         Object[] GameobjectList = Resources.FindObjectsOfTypeAll(typeof(GameObject));
         foreach (GameObject go in GameobjectList)
         {
@@ -55,24 +46,54 @@ public class MenuDislay : MonoBehaviour
                 }
                 if (go.tag == "Bat")
                 {
-                    s = "- Quatrième ailes";
+                    s = "- QuatriÃ¨me ailes";
                 }
                 if (go.tag == "Bag")
                 {
                     s = "- Sac";
-                }
-                nb_go++;
-                y = -0.015f * nb_go;
-                TextMeshPro text = Instantiate(ArborescenceBalise.GetComponent<TextMeshPro>(), menu.transform);
-                text.text = s;
-                text.transform.parent = menu.transform;
-                text.transform.position = new Vector3(ArborescenceBalise.position.x, ArborescenceBalise.position.y + y, ArborescenceBalise.position.z);
-                go.GetComponent<VirtualObject>().textArbo = text; 
-                if (SetLastObject.lastSelected == go)
-                {
-                    go.GetComponent<VirtualObject>().textArbo.color = Color.green;
-                }
+                }*/
+    }
+
+    public void creationArbo(List<ArboRessource> tree, string indent, int nb_go)
+    {
+     
+        Transform ArborescenceBalise = menu.transform.GetChild(4);
+        float y = 0.0f;
+        string s = "";
+        foreach (ArboRessource arbo in tree)
+        {
+            GameObject go = arbo.GetGameObject();
+            s = indent + go.GetComponent<VirtualObject>().GetName();
+            nb_go++;
+            Debug.Log(nb_go);
+            y = -0.015f * nb_go;
+            TextMeshPro text = Instantiate(ArborescenceBalise.GetComponent<TextMeshPro>(), menu.transform);
+            text.text = s;
+            text.transform.parent = menu.transform;
+            text.transform.position = new Vector3(ArborescenceBalise.position.x, ArborescenceBalise.position.y + y, ArborescenceBalise.position.z);
+            go.GetComponent<VirtualObject>().textArbo = text;
+            if (SetLastObject.lastSelected == go)
+            {
+                go.GetComponent<VirtualObject>().textArbo.color = Color.green;
             }
+            if (!arbo.childvide())
+            {
+                Debug.Log("Enfant");
+                indent = indent + "   "; 
+                creationArbo(arbo.GetChildren(), indent + ">", nb_go);
+                nb_go = nb_go + arbo.childCount();
+                indent = "";
+            }
+        }
+    }
+
+    public void suppressionArbo()
+    {
+        int numberOfChild = 5;
+        menu.transform.rotation = new Quaternion(0, 0, 0, 0);
+        for (int i = menu.transform.childCount; i > numberOfChild; i--)
+        {
+            Destroy(menu.transform.GetChild(i - 1).gameObject);
         }
     }
 }
